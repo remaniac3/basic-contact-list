3a35a976630b375e13aecf68c5b397ffe46796f8
R3MANIAC <50291272+remaniac3@users.noreply.github.com>
2020-04-22 19:49:37 +0800

    Styling WIP

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,37 +3,50 @@ import Card from "./Card";
 import contacts from "../contacts";
 import "./layout.css";
 
+function createCard(contact) {
+    return (
+        <Card
+            key={contact.id}
+            name={contact.name}
+            img={contact.profileImg}
+            species={contact.species}
+            job={contact.occupation}
+            email={contact.email}
+        />
+    );
+}
+
 function App() {
     return (
-        <div>
-            <h2>Contacts</h2>
-
-            <Card
-                name={contacts[0].name}
-                img={contacts[0].profileImg}
-                species={contacts[0].species}
-                job={contacts[0].occupation}
-                email={contacts[0].email}
-            />
-
-            <Card
-                name={contacts[1].name}
-                img={contacts[1].profileImg}
-                species={contacts[1].species}
-                job={contacts[1].occupation}
-                email={contacts[1].email}
-            />
-
-            <Card
-                name={contacts[2].name}
-                img={contacts[2].profileImg}
-                species={contacts[2].species}
-                job={contacts[2].occupation}
-                email={contacts[2].email}
-            />
-
+        <div className="container">
+            <h1>Contacts</h1>
+            {contacts.map(createCard)};
         </div>
     )
 }
 
 export default App;
+
+/* ==== NOTES ==== */
+    // Previous repetitive method.
+    // <Card
+    //     name={contacts[0].name}
+    //     img={contacts[0].profileImg}
+    //     species={contacts[0].species}
+    //     job={contacts[0].occupation}
+    //     email={contacts[0].email}
+    // />
+    // <Card
+    //     name={contacts[1].name}
+    //     img={contacts[1].profileImg}
+    //     species={contacts[1].species}
+    //     job={contacts[1].occupation}
+    //     email={contacts[1].email}
+    // />
+    // <Card
+    //     name={contacts[2].name}
+    //     img={contacts[2].profileImg}
+    //     species={contacts[2].species}
+    //     job={contacts[2].occupation}
+    //     email={contacts[2].email}
+    // />
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,12 +7,12 @@ import EmailAddress from "./EmailAddress";
 
 function Card(props) {
     return (
-        <div>
-            <div>
-                <h3>{props.name}</h3>
+        <div className="contacts">
+            <div className="avatar">
                 <ProfilePic img={props.img} />
             </div>
-            <div>
+            <div className="details">
+                <h3>{props.name}</h3>
                 <Details detailInfo={props.species} />
                 <Details detailInfo={props.job} />
                 <EmailAddress emailAdd={props.email} />
