56ba215be22cf53a1c78a2581cf6cdf1c23658dc
R3MANIAC <50291272+remaniac3@users.noreply.github.com>
2020-04-23 17:23:45 +0800

    removed unused lines

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { checkPropTypes } from "prop-types";
 
 function Details(props) {
     return <p>{props.detailInfo}</p>
--- src/components/EmailAddress.jsx
+++ src/components/EmailAddress.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { checkPropTypes } from "prop-types";
 
 function EmailAddress(props) {
     return <p><a href={`mailto:${props.emailAdd}`}>{props.emailAdd}</a></p>
